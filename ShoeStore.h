8140b58e04fcc641604152881dbb58e7bb958e96
gokhanesque <sgokhanaydogdu@gmail.com>
2014-01-03 01:08:03 +0200

    Create ShoeStore.h

---
+++ ShoeStore.h
@@ -0,0 +1,10 @@
+ 1 #include "Store.h"
+    2 
+   13 class ShoeStore : public Store {
+   14 
+   15 public:
+   16 
+   26     ShoeStore(string name) : Store(name){};
+   27 
+   32     void printItems();
+   33 };
